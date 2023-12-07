c116077049744a7acdc2074b3363d64310dbdc77
Nishant004 <nishantbhagat4@gmail.com>
2023-12-07 14:32:50 +0530

    202.Happy Number

---
+++ leetcode/202. Happy Number.py
@@ -0,0 +1,32 @@
+n = 70
+used=[]
+
+while n > 0 :
+    storge=0
+    while n>0:
+        temp = n % 10
+        storge += temp ** 2
+        n = n // 10
+
+    if (storge in used):
+        print("False")
+
+    else:
+        used.append(storge)
+        print(storge)
+        print(used)
+    if (storge == 1):
+
+        print("True")
+
+    n = storge
+
+
+print("False")
+
+
+
+
+
+
+
