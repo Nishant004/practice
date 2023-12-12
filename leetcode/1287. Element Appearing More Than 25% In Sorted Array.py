d242a9982624672baf574350cf1dc0e9e5c42c1c
Nishant004 <nishantbhagat4@gmail.com>
2023-12-12 13:33:39 +0530

    1287. Element Appearing More Than 25% In Sorted Array

---
+++ leetcode/1287. Element Appearing More Than 25% In Sorted Array.py
@@ -0,0 +1,14 @@
+# arr = [1,2,2,6,6,6,6,7,10]
+arr = [1,1]
+
+dict1={}
+
+for i in arr:
+    if i in dict1:
+        dict1[i] +=1
+    else:
+        dict1[i] =1
+    if dict1[i] > (len(arr)//4):
+        print(i)
+# print(dict1)
+# print(len(arr))
